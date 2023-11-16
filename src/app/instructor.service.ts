a85fb6fdf329586501c36f0ca0a32f0a6971f6ff
ahmed-mabrouk902 <mabrouk.ahmed@esprit.tn>
2023-11-16 02:37:54 +0000

    angular

--- src/app/instructor.service.ts
+++ src/app/instructor.service.ts
@@ -8,16 +8,16 @@ import { Instructor } from './instructor.model';
 
 })
 export class InstructorService {
-  readonly apiUrl = 'http://193.168.33.10:8089/api/instructor'; // Replace with your Spring Boot server URL
+  readonly apiUrl = 'http://193.168.33.10:8089/api'; // Replace with your Spring Boot server URL
 
   constructor(private http: HttpClient) { }
 
   getAllInstructors(): Observable<Instructor[]> {
     console.log('Fetching instructors...');
-    return this.http.get<Instructor[]>(this.apiUrl + '/all');
+    return this.http.get<Instructor[]>(this.apiUrl + '/instructor/all');
   }
 
   addInstructor(instructor: Instructor): Observable<Instructor> {
-    return this.http.post<Instructor>(this.apiUrl + '/add', instructor);
+    return this.http.post<Instructor>(this.apiUrl + '/instructor/add', instructor);
   }
 }
